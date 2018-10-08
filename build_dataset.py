95b8d8c330f523176a30ff029e1a1b744d5b626d
Yuol96 <huangyc96@gmail.com>
2018-10-08 17:52:46 +0800

    rearrange the code

--- build_dataset.py
+++ build_dataset.py
@@ -6,12 +6,14 @@ import numpy as np
 from PIL import Image
 import cv2
 from tqdm import tqdm
+import datetime
 
 from torchvision import transforms
 
 class FlagBuilder:
-	def __init__(self, root_dir='./'):
+	def __init__(self, root_dir='./', name=""):
 		self.root_dir = Path(root_dir)
+		(self.root_dir/'data').mkdir(exist_ok=True)
 
 		self.transforms = transforms.Compose([
 	                transforms.ToPILImage(),
@@ -28,8 +30,37 @@ class FlagBuilder:
 	def save_image(self, img, img_path):
 		cv2.imwrite(img_path, img)
 
+	def build(self, name, iter_img_paths, exist_ok=False, num_train_classes=100, num_test_classes=100, 
+				num_flags=5, scaleRange=(0.07,0.11)):
+		self.dataset_dir = self.root_dir / 'data' / name
+		try:
+			self.dataset_dir.mkdir(exist_ok=exist_ok)
+		except:
+			raise Exception("{} exists!".format(name))
+		self.build_randomGallery(num_train_classes, num_test_classes)
+
+		print("Built train and test galleries!")
+
+		iter_img_paths = list(iter_img_paths)
+		for phase in ['train','test']:
+			self.build_randomDataset(num_flags, iter_img_paths, phase, scaleRange)
+			print('Built {} dataset!'.format(phase))
+
+		print('Done!')
+
+
+	def build_randomGallery(self, num_train_classes=1, num_test_classes=1, size=(120,120)):
+		flagGen = RandomFlagGenerator()
+		for phase in ['train','test']:
+			num_classes = eval('num_{}_classes'.format(phase))
+			gallery_dir = self.dataset_dir / phase / 'gallery'
+			gallery_dir.mkdir(exist_ok=True, parents=True)
+			for idx in range(num_classes):
+				img = flagGen.getRandomFlag(size)
+				self.save_image(img, str(gallery_dir/'{}.png'.format(idx)))
+
 	def load_gallery(self, phase):
-		gallery_dir = self.root_dir / 'data' / 'flag' / 'gallery' / phase
+		gallery_dir = self.dataset_dir / phase / 'gallery'
 		gallery = {}
 		for path in gallery_dir.glob('*'):
 			ind = int(path.name.split('.')[0])
@@ -37,7 +68,7 @@ class FlagBuilder:
 			gallery[ind] = img
 		return gallery
 
-	def random_insert_flag(self, img, gallery, flagIdx, scale=0.07, prevbboxes=None):
+	def random_insert_flag(self, img, flag, scale=0.07, prevbboxes=None):
 		size = min(img.shape[:2])
 		flag_height = size * scale
 		flag_scale = np.random.uniform(0.8, 1.2)
@@ -55,36 +86,29 @@ class FlagBuilder:
 						continue
 			break
 
-		flag = gallery[flagIdx].copy()
 		flag = np.array(self.transforms(flag))
 		flag = cv2.resize(flag, dsize=(flag_width, flag_height))
 
 		mask = (flag==0)
-		# bbox = {
-		# 	'xmin': pos[1],
-		# 	'ymin': pos[0],
-		# 	'xmax': pos[1]+flag_width,
-		# 	'ymax': pos[0]+flag_height,
-		# }
-		# img[bbox['ymin']:bbox['ymax'], bbox['xmin']:bbox['xmax']] *= mask
-		# img[bbox['ymin']:bbox['ymax'], bbox['xmin']:bbox['xmax']] += flag
 		img[bbox[1]:bbox[3], bbox[0]:bbox[2]] *= mask
 		img[bbox[1]:bbox[3], bbox[0]:bbox[2]] += flag
 		return img, bbox
 
-	def random_insert_multiflags(self, img, gallery, num_flags, scaleRange=(0.07, 0.15)):
+	def random_insert_multiflags(self, img, gallery, num_flags, scaleRange):
 		flagIndices = list(gallery.keys())
 		bboxList = []
 		for __ in range(num_flags):
 			scale = np.random.uniform(scaleRange[0], scaleRange[1])
 			flagIdx = np.random.choice(flagIndices)
-			img, bbox = self.random_insert_flag(img, gallery, flagIdx, scale=scale, prevbboxes=bboxList)
+			flag = gallery[flagIdx].copy()
+			img, bbox = self.random_insert_flag(img, flag, scale=scale, prevbboxes=bboxList)
 			bboxList.append(bbox)
 		return img, bboxList
 
-	def build_randomDataset(self,num_flags,iter_img_paths,phase, scaleRange=(0.07, 0.15)):
-		imgs_dir = self.root_dir / 'data' / 'flag' / 'imgs' / phase
-		imgs_dir.mkdir(parents=True, exist_ok=True)
+	def build_randomDataset(self,num_flags,iter_img_paths,phase, scaleRange):
+		imgs_dir = self.dataset_dir / phase / 'imgs'
+		imgs_dir.mkdir()
+
 		gallery = self.load_gallery(phase)
 		infoList = []
 		for idx, path in tqdm(enumerate(iter_img_paths)):
@@ -101,30 +125,23 @@ class FlagBuilder:
 			infoList.append(info)
 		import json
 		jsonStr = json.dumps(infoList)
-		with open(str(self.root_dir / 'data' / 'flag' / 'infoList.json'), 'w') as hd:
+		with open(str(self.dataset_dir / phase / 'infoList.json'), 'w') as hd:
 			hd.write(jsonStr)
-		print("generated {} dataset!".format(phase))
 
-	def build_randomGallery(self, num_train_classes=1, num_test_classes=1, size=(120,120)):
-		flagGen = RandomFlagGenerator()
-		dataset_dir = self.root_dir / 'data' / 'flag'
-		gallery_dir = dataset_dir / 'gallery'
-		(gallery_dir/'train').mkdir(exist_ok=True, parents=True)
-		(gallery_dir/'test').mkdir(exist_ok=True, parents=True)
-		for idx in range(num_train_classes):
-			img = flagGen.getRandomFlag(size)
-			cv2.imwrite(str(gallery_dir/'train'/'{}.png'.format(idx)), img)
-		for idx in range(num_test_classes):
-			img = flagGen.getRandomFlag(size)
-			cv2.imwrite(str(gallery_dir/'test'/'{}.png'.format(idx)), img)
+
 
 
 
 
 if __name__ == '__main__':
 	builder = FlagBuilder('/home/huangyucheng/MYDATA/dl-github/Siamese-RPN')
-	iter_img_paths = Path('/home/huangyucheng/MYDATA/DATASETS/PASCAL_VOC/VOCdevkit/VOC2007/JPEGImages').glob('*00.jpg')
-	builder.build_randomDataset(5, iter_img_paths, 'train')
+	iter_img_paths = Path('/home/huangyucheng/MYDATA/DATASETS/PASCAL_VOC/VOCdevkit/VOC2007/JPEGImages').glob('*.jpg')
+	builder.build('5flags', iter_img_paths)
+
+	# builder = FlagBuilder('/home/huangyucheng/MYDATA/dl-github/Siamese-RPN')
+	# iter_img_paths = Path('/home/huangyucheng/MYDATA/DATASETS/PASCAL_VOC/VOCdevkit/VOC2007/JPEGImages').glob('*00.jpg')
+	# builder.build_randomDataset(5, iter_img_paths, 'train')
+
 	# import argparse
 	# parser = argparse.ArgumentParser()
 	# parser.add_argument('--gallery', default=False, action="store_true")
