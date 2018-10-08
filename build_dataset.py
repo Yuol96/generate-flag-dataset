af767863b7e44845faeb3fdc4fe7ff81703846c5
Yuol96 <huangyc96@gmail.com>
2018-10-08 14:44:44 +0800

    add some config

--- build_dataset.py
+++ build_dataset.py
@@ -1,11 +1,12 @@
 from flag.randomflag import RandomFlagGenerator
 from pathlib import Path
 
-def build_randomGallery(num_train_classes=1, num_test_classes=1, dataset_dir='./data/', size=(120,120)):
+def build_randomGallery(num_train_classes=1, num_test_classes=1, root_dir='./data/', size=(120,120)):
 	import cv2
 
 	flagGen = RandomFlagGenerator()
-	dataset_dir = Path(dataset_dir)
+	root_dir = Path(root_dir)
+	dataset_dir = root_dir / 'data' / 'flag'
 	gallery_dir = dataset_dir / 'gallery'
 	(gallery_dir/'train').mkdir(exist_ok=True, parents=True)
 	(gallery_dir/'test').mkdir(exist_ok=True, parents=True)
@@ -14,7 +15,14 @@ def build_randomGallery(num_train_classes=1, num_test_classes=1, dataset_dir='./
 		cv2.imwrite(str(gallery_dir/'train'/'{}.png'.format(idx)), img)
 	for idx in range(num_test_classes):
 		img = flagGen.getRandomFlag(size)
-		cv2.imwrite(str(gallery_dir/'test'/'{}.png'.format(idx+num_train_classes)), img)
+		cv2.imwrite(str(gallery_dir/'test'/'{}.png'.format(idx)), img)
 
 if __name__ == '__main__':
-	build_randomGallery(100, 100)
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--gallery', default=False, action="store_true")
+	parser.add_argument('--dir', default="./data/")
+	parser.add_argument("--size", default=120, type=int)
+	args = parser.parse_args()
+	if args.gallery:
+		build_randomGallery(100, 100, root_dir=args.dir, size=(args.size, args.size))
